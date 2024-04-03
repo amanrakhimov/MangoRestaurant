25aa573f7378a2afc520fc05d581f4efa1834889
Аман Рахимов <rakhimov.aman1@kaznu.kz>
2024-04-03 14:41:13 +0500

    'LoginLogout'

--- Mango.Web/Program.cs
+++ Mango.Web/Program.cs
@@ -14,6 +14,26 @@ namespace Mango.Web
             SD.ProductAPIBase = builder.Configuration["ServiceUrls:ProductAPI"];
             builder.Services.AddScoped<IProductService, ProductService>();
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = "Cookies";
+                options.DefaultChallengeScheme = "oidc";
+            })
+                .AddCookie("Cookies", c => c.ExpireTimeSpan = TimeSpan.FromMinutes(100))
+                .AddOpenIdConnect("oidc", options =>
+                {
+                    options.Authority = builder.Configuration["ServiceUrls:IdentityAPI"];
+                    options.GetClaimsFromUserInfoEndpoint = true;
+                    options.ClientId = "mango";
+                    options.ClientSecret = "secret";
+                    options.ResponseType = "code";
+                    options.TokenValidationParameters.NameClaimType = "name";
+                    options.TokenValidationParameters.RoleClaimType = "role";
+                    options.Scope.Add("mango");
+                    options.SaveTokens = true;
+                });
+
             builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
             var app = builder.Build();
 
@@ -29,7 +49,7 @@ namespace Mango.Web
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.MapControllerRoute(
